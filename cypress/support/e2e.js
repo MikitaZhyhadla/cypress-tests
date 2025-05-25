22756a48d76ed36c964dcde3e3a012418e684267
Mikita Zhyhadla <nikizihadlo93@gmail.com>
2025-05-25 22:57:22 +0200

    feat: define global variables in e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,11 @@
 // e2e support file
-import './commands'
+import './commands';
+import { l10n } from './l10n.json';
+import { reqs } from './requirements';
+import { urls } from './urls';
+import { selectors } from './selectors';
+
+Cypress.env('l10n', l10n);
+Cypress.env('reqs', reqs);
+Cypress.env('urls', urls);
+Cypress.env('selectors', selectors);
