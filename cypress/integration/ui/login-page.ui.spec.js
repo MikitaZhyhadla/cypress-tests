6f1f1dff975c8839210aab5bbd1269b8deeb7f64
Mikita Zhyhadla <nikizihadlo93@gmail.com>
2025-05-25 23:09:24 +0200

    Implement valid login test case with bug reference

--- cypress/integration/ui/login-page.ui.spec.js
+++ cypress/integration/ui/login-page.ui.spec.js
@@ -1,6 +1,16 @@
 // login-page.ui.spec.js
 
 describe('Login Page', { testIsolation: false }, () => {
+  context('Valid login', () => {
+    it('Should login successfully with standard user credentials', () => {
+      cy.visit('/');
+      cy.get(selectors.login.usernameInput).type(envUsers.standard.username);
+      cy.get(selectors.login.passwordInput).type(envUsers.standard.password);
+      cy.get(selectors.login.loginButton).click();
+      cy.url().should('include', '/inventory.html');
+    });
+  });
+
   context('Empty credentials', () => {
     it.skip('Should show correct error when both fields are empty', () => {
       // TODO: https://github.com/MikitaZhyhadla/cypress-tests/issues/2
@@ -9,10 +19,6 @@ describe('Login Page', { testIsolation: false }, () => {
   });
 
   context.skip('Locked out user', () => {
-    // Not implemented yet
-  });
-
-  context.skip('Valid login', () => {
     // Not implemented yet
   });
 
