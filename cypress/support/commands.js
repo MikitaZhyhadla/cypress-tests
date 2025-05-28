2984378edad1ad8b93e9a96146ba29ca9565a113
Mikita Zhyhadla <nikizihadlo93@gmail.com>
2025-05-28 23:02:34 +0200

    feat: add custom command cy.loginAs(user)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,11 @@
-// custom Cypress commands can be added here
+// cypress/support/commands.js
+
+// Custom command to perform login with given user credentials
+Cypress.Commands.add('loginAs', (user) => {
+  cy.visit('/');
+  cy.get(Cypress.env('selectors').login.usernameInput).type(user.username);
+  cy.get(Cypress.env('selectors').login.passwordInput).type(user.password);
+  cy.get(Cypress.env('selectors').login.loginButton).click();
+});
+
+// You can add more custom commands below as needed
