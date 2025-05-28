308bac62a88c2a99fe68468c879309f6b8bc7ae4
Mikita Zhyhadla <nikizihadlo93@gmail.com>
2025-05-28 23:11:41 +0200

    test: split Gherkin steps into separate lines in login test

--- cypress/integration/ui/login-page.ui.spec.js
+++ cypress/integration/ui/login-page.ui.spec.js
@@ -1,35 +1,44 @@
 // login-page.ui.spec.js
 
 describe('Login Page', { testIsolation: false }, () => {
+  context('Valid Login', () => {
+    beforeEach(() => {
+      cy.visit('/');
+      cy.loginAs(Cypress.env('users').standard);
+    });
+
+    it('Then user should be redirected to inventory page', () => {
+      // Given login page
+      // When user logs in with standard credentials
+      // Then user should be redirected to inventory page
+      cy.url().should('include', '/inventory.html');
+    });
+  });
+
   context('Empty credentials', () => {
-    it.skip(`Given I am on login page
-When I submit the form with empty fields
-Then I should see an error message about missing credentials`, () => {
+    it.skip('Should show correct error for empty credentials', () => {
+      // Given login form
+      // When user submits without input
+      // Then should show correct error for empty fields
       // TODO: https://github.com/MikitaZhyhadla/cypress-tests/issues/2
       // Expected message: "Epic sadface: Username and password are required"
     });
   });
 
   context.skip('Locked out user', () => {
-    it.skip(`Given I am on login page
-When I log in as a locked out user
-Then I should see a locked out message`, () => {
-      // Not implemented yet
-    });
-  });
-
-  context.skip('Valid login', () => {
-    it.skip(`Given I am on login page
-When I enter valid credentials
-Then I should be redirected to the inventory page`, () => {
+    it.skip('Should show locked out error', () => {
+      // Given login form
+      // When user logs in with locked out user
+      // Then should show locked out error
       // Not implemented yet
     });
   });
 
   context.skip('Invalid credentials', () => {
-    it.skip(`Given I am on login page
-When I enter incorrect username or password
-Then I should see an error message`, () => {
+    it.skip('Should show invalid login error', () => {
+      // Given login form
+      // When user submits incorrect credentials
+      // Then should show invalid login error
       // Not implemented yet
     });
   });
