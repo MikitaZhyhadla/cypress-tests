fc1464e6b65ce2b84b99f746b57dcf2d64aaf4f0
Mikita Zhyhadla <nikizihadlo93@gmail.com>
2025-06-01 00:16:31 +0200

    feat: add users from qa-users.json to Cypress.env in e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -4,8 +4,10 @@ import l10n from './l10n.json';
 import { reqs } from './requirements';
 import { urls } from './urls';
 import { selectors } from './selectors';
+import users from '../sensitive-data/qa-users.json';
 
 Cypress.env('l10n', l10n);
 Cypress.env('reqs', reqs);
 Cypress.env('urls', urls);
 Cypress.env('selectors', selectors);
+Cypress.env('users', users);
