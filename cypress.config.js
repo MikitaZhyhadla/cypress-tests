3fcba0fc972fc03e5ce1ac9318dfdade16b4baea
Mikita Zhyhadla <nikizihadlo93@gmail.com>
2025-06-01 01:26:37 +0200

    fix: update specPattern in Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     supportFile: 'cypress/support/e2e.js',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/integration/**/*.js',
   },
 })
