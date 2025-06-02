f695634875e9db4e73562f1adabc85012320ed1f
Mikita Zhyhadla <nikizihadlo93@gmail.com>
2025-06-02 22:58:43 +0200

    add illustrative constants with note to requirements.js

--- cypress/support/requirements.js
+++ cypress/support/requirements.js
@@ -1,4 +1,9 @@
 // requirements.js
+// NOTE:
+// The values below are illustrative examples added as part of Learning Session 2
+// They are not based on confirmed business requirements.
+// Please update or remove them once real requirements are available.
+
 export const reqs = {
   minPasswordLength: 6,
   maxUsernameLength: 20
