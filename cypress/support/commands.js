200e80f178f7d5376d4c2a2ea65f5f2e13b2422e
Mikita Zhyhadla <nikizihadlo93@gmail.com>
2025-06-30 01:15:29 +0200

    Refactor login test and config: use env-users.json for credentials

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,12 @@
-// cypress/support/commands.js
-
 // Custom command to perform login with given user credentials
 Cypress.Commands.add('loginAs', (user) => {
   const { username, password } = user;
   cy.visit('/');
-  cy.then(()=>{
+  cy.then(() => {
     cy.get(loginPage.usernameInput).type(username);
     cy.get(loginPage.passwordInput).type(password);
   });
-  cy.then(()=>{
+  cy.then(() => {
     cy.get(loginPage.loginButton).click();
   });
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,18 +1,13 @@
-// cypress/support/e2e.js
-
 import './commands';
 import l10n from './l10n.json';
-import  reqs  from './requirements';
-import  urls  from './urls';
-import  selectors  from './selectors';
-import users from '../sensitive-data/qa-users.json';
+import reqs from './requirements';
+import urls from './urls';
+import selectors from './selectors';
 
 global.loginPage = selectors.loginPage;
-
 global.cartPage = selectors.cartPage;
 global.l10n = l10n;
 global.urls = urls;
 global.selectors = selectors;
-global.users = users;
 global.reqs = reqs;
-
+global.users = Cypress.env('users');
