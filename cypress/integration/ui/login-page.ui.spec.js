b2cee62f57c19348aa78fe07de817e7172246466
Mikita Zhyhadla <nikizihadlo93@gmail.com>
2025-07-20 23:23:37 +0200

    rewrite test titles, reorder tests and add locked out user error validation

--- cypress/integration/ui/login-page.ui.spec.js
+++ cypress/integration/ui/login-page.ui.spec.js
@@ -1,47 +1,59 @@
-describe('Login Page: Given no preconditions', { testIsolation: false }, () => {
-  context('Login Page: When user navigates to the page', () => {
+describe('Login Page: Given user is on the login page', { testIsolation: false }, () => {
+  context('Login Page: When user reviews the login page', () => {
     before(() => {
-      cy.visit('/');
-    });
+      cy.visit('/')
+    })
 
     it('Login Page: Then login page title should be visible', () => {
-      cy.get(loginPage.title).should('have.text', l10n.login.title).and('be.visible');
-    });
+      cy.get(loginPage.title).should('have.text', l10n.login.title).and('be.visible')
+    })
 
     it('Login Page: Then username input should be visible', () => {
-      cy.get(loginPage.usernameInput).should('be.visible');
-    });
+      cy.get(loginPage.usernameInput).should('be.visible')
+    })
 
     it('Login Page: Then password input should be visible', () => {
-      cy.get(loginPage.passwordInput).should('be.visible');
-    });
+      cy.get(loginPage.passwordInput).should('be.visible')
+    })
 
     it('Login Page: Then login button should be visible', () => {
-      cy.get(loginPage.loginButton).should('be.visible');
-    });
-  });
-
-  context('Login Page: When user submits valid standard credentials', () => {
-    before(() => {
-      cy.loginAs(Cypress.env('users').standardUser);
-    });
-
-    it('Login Page: Then user should be redirected to inventory page', () => {
-      cy.url().should('include', '/inventory.html');
-    });
-  });
+      cy.get(loginPage.loginButton).should('be.visible')
+    })
+  })
 
   context('Login Page: When user submits empty credentials', () => {
     it.skip('Then user should see error for empty fields', () => {
       // TODO: https://github.com/MikitaZhyhadla/cypress-tests/issues/2
-    });
-  });
+    })
+  })
 
-  context.skip('Login Page: When user submits locked out credentials', () => {
-    it.skip('Then user should see locked out error', () => {});
-  });
+  context('Login Page: When user submits locked out credentials', () => {
+    before(() => {
+      cy.visit('/')
+    })
+
+    it('Then user should see locked out error', () => {
+      cy.get(loginPage.usernameInput).type('locked_out_user')
+      cy.get(loginPage.passwordInput).type('secret_sauce')
+      cy.get(loginPage.loginButton).click()
+
+      cy.get(loginPage.errorContainer)
+        .should('be.visible')
+        .and('contain.text', 'Sorry, this user has been locked out.')
+    })
+  })
 
   context.skip('Login Page: When user submits invalid credentials', () => {
-    it.skip('Then user should see invalid login error', () => {});
-  });
-});
+    it.skip('Then user should see invalid login error', () => {})
+  })
+
+  context('Login Page: When user submits valid standard credentials', () => {
+    before(() => {
+      cy.loginAs(Cypress.env('users').standardUser)
+    })
+
+    it('Login Page: Then user should be redirected to inventory page', () => {
+      cy.url().should('include', '/inventory.html')
+    })
+  })
+})
