46253e56939887ec12335fad5491be2874cc6916
Mikita Zhyhadla <nikizihadlo93@gmail.com>
2025-07-20 23:26:29 +0200

    add missing errorContainer selector for login page

--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -1,15 +1,14 @@
-// cypress/support/selectors.js
-
 const selectors = {
   loginPage: {
     title: '.login_logo',
     usernameInput: '#user-name',
     passwordInput: '#password',
     loginButton: '#login-button',
+    errorContainer: '[data-test="error"]',
   },
   cartPage: {
     continueShopping: '[data-test="continue-shopping"]',
-  }
-};
+  },
+}
 
-export default selectors;
+export default selectors
